ebd38ad586b8d2cb5c00ab5ae106069e534dbac2
fjalia <farhaan.jalia@gmail.com>
2016-12-15 21:01:10 -0500

    Added code for returning max matched prefix if entrie pattern is not found.

--- SuffixArrayCode.cpp
+++ SuffixArrayCode.cpp
@@ -8,7 +8,7 @@ using namespace std;
 #include <sys/stat.h>
 #include <unistd.h>
 #include "rank9b.h"
-
+#include <ctime>
 #define VALUE_A 0
 #define VALUE_T 1
 #define VALUE_G 2
@@ -40,6 +40,11 @@ int main(int argc, char *argv[]) {
 	char *temp_buf = NULL;
 	int read = 0;
 	struct stat stat_buf;
+	unsigned long max_index = 0;
+	unsigned long max_l = 0;
+	unsigned long match_l = 0;
+	clock_t Start = clock();
+	clock_t End;
 
 	temp_buf = (char *)malloc(500000);
 	if (temp_buf == NULL)
@@ -68,37 +73,22 @@ int main(int argc, char *argv[]) {
 	}
 	in_size = actual_size;
 	packed_size = in_size/4 + 1;
-	/*for (int i = 0; i < actual_size; i++) {
-		if (in[i] != '#') {
-			in_size++;
-		}
-	}
-	packed_size = in_size/4 + 1;
-	bit_arr = (uint64_t *) malloc(in_size/8+8);
-	in = (char *) malloc(actual_size+1);
-	strcpy(in,in1);
-	for (int i = 0, j = 0; i < actual_size; i++, j++) {
-		if (in[i] != '#' && j != i) {
-			in[j] = in[i];
-		}
-		else if(in[i] == '#') {
-			j--;
-			//set j'th bit of bit_arr
-			bit_arr[j/64] |= (1 << (j%64));
-		}
-	}*/
 	rds = new rank9b(bit_arr, in_size);
 
 	SA = (saidx_t *) malloc(actual_size * sizeof(saidx_t));
 	if (SA == NULL)
 		return 1;
+
+	Start = clock();
 	saint_t ret = divsufsort((unsigned char *) in, SA, in_size);
+	cout<<"Time to create SA = "<<(clock() - Start)/CLOCKS_PER_SEC<<"s\n";
 	
 	data = (unsigned char *) malloc(packed_size);
 	if (data == NULL) {
 		return 1;
 	}
 	memset(data, 0, packed_size);
+	Start = clock();
 	for (int i = 0; i < in_size; i++) {
 		if (in[i] == 'A') {
 			data[i/4] = data[i/4] | (VALUE_A <<((i % 4) * 2));
@@ -113,16 +103,7 @@ int main(int argc, char *argv[]) {
                         data[i/4] = data[i/4] | (VALUE_C <<((i % 4) * 2));
                 }
 	}
-	/*for (int i = 0; i < in_size; i++) {
-		if (get_value(data, i) == VALUE_A) 
-			cout<<"A";
-		else if (get_value(data, i) == VALUE_T)
-                        cout<<"T";
-		else if (get_value(data, i) == VALUE_G)
-                        cout<<"G";
-		else
-			cout<<"C";
-	}*/
+	cout<<"Time to convert to packed format = "<<(clock() - Start)/CLOCKS_PER_SEC<<"s\n";
 	free(in);
 	free(temp_buf);
 	while(1) {
@@ -130,13 +111,17 @@ int main(int argc, char *argv[]) {
 		cin>>pat;
 		if (pat.at(0) == 'q')
 			break;
-		
+		Start = clock();
+		low = 0;
 		high = in_size - 1;
 		int i;
+		max_index = 0;
+		max_l = 0;
 		while (low <= high)
 		{
 			mid = low + (high - low) / 2;
 			start_rank = rds->rank(SA[mid]);
+			match_l = 0;
 			for (i = 0; i < pat.length() && SA[mid] + i < in_size; i++)
 			{
 				val = get_value(data, SA[mid] + i);
@@ -153,6 +138,11 @@ int main(int argc, char *argv[]) {
 					cval = 'C';
 				}
 				if (cval == pat.at(i) && start_rank == rds->rank(SA[mid] + i)) {
+					match_l++;
+					if (match_l > max_l) {
+                                                max_l = match_l;
+                                                max_index = SA[mid];
+                                        }
 					continue;
 				}
 				else {
@@ -161,6 +151,7 @@ int main(int argc, char *argv[]) {
 			}
 			if (i == pat.length())
 			{
+				End = clock();
 				cout << "Pattern found at index "<< SA[mid]<<"\n";
 				break;
 			}
@@ -172,6 +163,11 @@ int main(int argc, char *argv[]) {
 				low = mid + 1;
 			}
 		}
+		if (max_l < pat.length()) {
+			End = clock();
+			cout<<"Exact match not found. Length "<<max_l<<" prefix of pattern found is : "<<pat.substr(0,max_l)<<"\n";
+		}
+		cout<<"Time to search = "<<((double)((double)End-(double)Start)/(double)CLOCKS_PER_SEC)*(double)1000<<"ms\n";
 	}
 	return 0;
 }
